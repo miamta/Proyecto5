3bf3138135fdc912a92174e94cf1a2b52ee62942
Miguel Amores Tamarit <miamta@floridauniversitaria.es>
2018-02-06 12:31:22 +0100

    proyecto5

---
+++ InsertarUsuarios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Insertar Usuarios</title>
+  </head>
+  <body>
+    <button><a href="Index.php">Formulario</a></button>
+    <button><a href="listadoUsuarios.php">ListadoUsuario</a></button>
+
+
+
+    <h3>Usuarios insertados:</h3>
+    <?php
+    // conexion a la BD
+  include "juegodb.php";
+  $juego = new juegodb();
+
+   $resultado = $juego->InsertarUsuarios($_POST["Nombre"],$_POST["Apellidos"],$_POST["Edad"],$_POST["Curso"],$_POST["Puntuacion"]);
+   // Muestra el ultimo jugador insertado
+   $resultado = $juego->mostrarUltimo($_POST["Nombre"]);
+   // echo "Usuario Insertado correctamente";
+   foreach ($resultado as $fila){
+ 		echo "Nombre: ".$fila['Nombre']."<br>";
+ 	}
+     ?>
+
+  </body>
+</html>
